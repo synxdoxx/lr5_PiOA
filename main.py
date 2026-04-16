633fd52471e8f020d6375b404ed53cab806c7b1d
irrrrrrrrrrr <95918722+synxdoxx@users.noreply.github.com>
2026-04-16 20:25:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import Levenshtein
 def normalize(text):
     return text.strip().lower()
 
-def find_matches(small_list, big_list, porog=2):
-    full_matches = []
+def find_matches(small_list, big_list, porog=3):
+    exact_matches = []
     other_matches = []
 
     normalized_big = [
@@ -19,10 +19,9 @@ def find_matches(small_list, big_list, porog=2):
 
         for big_item in normalized_big:
             if small_name == big_item["name"]:
-                full_matches.append({
-                    "small_id": small_item["id"],
-                    "big_id": big_item["id"],
-                    "name": small_item["name"]
+                exact_matches.append({
+                    "Название": small_item["name"],
+                    "ID в базе данных": big_item["id"]
                 })
                 found_exact = True
                 break
@@ -35,18 +34,18 @@ def find_matches(small_list, big_list, porog=2):
 
                 if distance <= porog:
                     variants.append({
-                        "big_id": big_item["id"],
-                        "name": big_item["name"],
-                        "distance": distance
+                        "ID в базе данных": big_item["id"],
+                        "Название": big_item["name"],
+                        "Разница": distance
                     })
 
             if variants:
                 other_matches.append({
-                    "name": small_item["name"],
-                    "variants": sorted(variants, key=lambda x: x["distance"])
+                    "Название": small_item["name"],
+                    "Возможные варианты": sorted(variants, key=lambda x: x["Разница"])
                 })
 
-    return full_matches, other_matches
+    return exact_matches, other_matches
 
 small_list = [
     {"id": 1, "name": "iphone 13"},
@@ -61,12 +60,10 @@ big_list = [
     {'id': 104, 'name': 'Xiaomi Blue'},
 ]
 
-full, other = find_matches(small_list, big_list, porog=3)
+exact, other = find_matches(small_list, big_list, porog=3)
 
-print("FULL MATCHES:")
-for item in full:
-    print(item)
+print("Точные совпадения:")
+print(*exact, sep="\n")
 
-print("OTHER MATCHES:")
-for item in other:
-    print(item)
+print("Возможные совпадения:")
+print(*other, sep="\n")
